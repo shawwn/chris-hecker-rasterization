e13f26dc4e577d8aef8297d1ec72adaf32fc665a
Shawn Presser <shawnpresser@gmail.com>
2020-06-22 20:24:38 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Software Rasterization
+
+This is a mirror of Chris Hecker's 5-part series on software rasterization: https://www.chrishecker.com/Miscellaneous_Technical_Articles
---
+++ Texture/TEXTURE.H
@@ -0,0 +1,31 @@
+/*----------------------------------------------------------------------------
+
+Texture Test Program - a cheesy test harness for texture mapping
+
+by Chris Hecker for my Game Developer Magazine articles.  See my homepage
+for more information.
+
+NOTE: This is a hacked test program, not a nice example of Windows programming.
+The texture mappers are the only part of this you should look at.
+
+This material is Copyright 1997 Chris Hecker, All Rights Reserved.
+It's for you to read and learn from, not to put in your own articles
+or books or on your website, etc.  Thank you.
+
+Chris Hecker
+checker@d6.com
+http://www.d6.com/users/checker
+
+*/
+
+/* Menu Items */
+#define MENU_ABOUT			0
+#define MENU_EXIT			1
+#define MENU_ANIMATE		2
+#define MENU_TIME			3
+#define MENU_TEST			4
+#define MENU_PIXELSPERSEC	5
+
+#define MENU_MAPPER		10
+#define MENU_VIEW		50
+
